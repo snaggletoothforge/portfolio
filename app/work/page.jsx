3f658b91f0e22c7f90a4c59bb772a87200eb2cc6
Tom Mitchell <tmitchell90@live.co.uk>
2026-02-10 10:33:02 +0100

    Updated work page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -12,30 +12,31 @@ export const metadata = {
 
 const workItems = [
   // Row 1: Snaggletooth intro
+  { type: 'casestudy', slug: 'digitickets' },
+   { type: 'image', src: '/images/designs/cadbury.webp', span: 2 },
+     { type: 'image', src: '/images/designs/slyme.png', span: 4 },
     { type: 'casestudy', slug: 'snaggletooth' },
       { type: 'video', src: '/images/designs/refined/optimised/trimmed-card-hover.webm', span: 2 },
-  { type: 'image', src: '/images/designs/refined/optimised/st-wide.webp', span: 4 },
-  { type: 'image', src: '/images/designs/refined/optimised/1.webp', span: 2 },
-  { type: 'video', src: '/images/designs/refined/optimised/loading.webm', span: 2 },
-  { type: 'image', src: '/images/designs/refined/optimised/brumble-large.webp', span: 4 },
+
+      { type: 'image', src: '/images/designs/refined/optimised/st-wide.webp', span: 4 },
+  { type: 'image', src: '/images/designs/refined/optimised/1.webp', span: 1 },
+    { type: 'image', src: '/images/designs/refined/optimised/brumble-large.webp', span: 3 },
+  { type: 'image', src: '/images/designs/searchflow.png', span: 4 },
+    { type: 'casestudy', slug: 'landmark' },
+  { type: 'casestudy', slug: 'metoffice' },
+
+              { type: 'image', src: '/images/designs/mo.png', span: 4 },
+        { type: 'quote', text: "Tom is one of the most grounded and effective leaders I've worked with. His work is high quality and thoughtful, with strong attention to detail and craft.", author: 'Amy Rushton', role: 'UX Designer, Landmark', span: 4 },
       { type: 'casestudy', slug: 'sidequest' },
                        { type: 'image', src: '/images/designs/refined/optimised/design-system.webp', span: 2 },
              { type: 'image', src: '/images/designs/refined/optimised/sq-wide.webp', span: 4 },
          { type: 'video', src: '/images/designs/refined/optimised/trimmed.webm', span: 1 },
   { type: 'quote', text: "Tom is a brilliant designer and UX researcher. He's my favourite person to have in an ideation session, exceptional at transforming rough fragments of ideas into fully fleshed out UI gold!", author: 'Ryan Smee', role: 'Frontend Developer, SideQuest' },
                    { type: 'video', src: '/images/designs/refined/optimised/psloader.webm', span: 1 },
-{ type: 'image', src: '/images/designs/refined/optimised/personas.webp', span: 4 },
-        { type: 'image', src: '/images/designs/refined/optimised/mobile-about-mockup.webp', span: 2 },
-        { type: 'video', src: '/images/designs/refined/optimised/hover-invr.webm', span: 2 },
-        { type: 'image', src: '/images/designs/refined/optimised/banternav.webp', span: 4 },
-        { type: 'video', src: '/images/designs/refined/optimised/sq-account.webm', span: 4 },
+                           { type: 'video', src: '/images/designs/refined/optimised/sq-account.webm', span: 4 },
+                   { type: 'image', src: '/images/designs/lumiya.png', span: 4 },
 
-    
-    { type: 'quote', text: "Tom is one of the most grounded and effective leaders I've worked with. His work is high quality and thoughtful, with strong attention to detail and craft.", author: 'Amy Rushton', role: 'UX Designer, Landmark' },
-
-  { type: 'casestudy', slug: 'metoffice' },
-    { type: 'casestudy', slug: 'landmark' },
-      { type: 'casestudy', slug: 'digitickets' },
+      
 ];
 
 export default function Work() {
